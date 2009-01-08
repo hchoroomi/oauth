38637c077d13af18f4549988194e536151334042
hchoroomi <hchoroomi@gmail.com>
2009-01-08 19:29:39 +0330

    Fixing base signatures including unicode characters

--- lib/oauth/request_proxy/action_controller_request.rb
+++ lib/oauth/request_proxy/action_controller_request.rb
@@ -37,7 +37,7 @@ module OAuth::RequestProxy
 
       unless options[:clobber_request]
         params << header_params.to_query
-        params << request.query_string unless request.query_string.blank?
+        params << CGI.unescape(request.query_string) unless request.query_string.blank?
         if request.content_type == Mime::Type.lookup("application/x-www-form-urlencoded")
           params << CGI.unescape(request.raw_post)
         end
